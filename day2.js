6cd3ca82927d9c10b966df9083146423680b5b67
JasminConterioSW <Jasmin.Conterio@softwire.com>
2022-03-18 15:26:25 +0000

    Completed part2 of day 2

--- day2.js
+++ day2.js
@@ -1,39 +1,57 @@
-inputProgram = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,10,19,23,1,23,9,27,1,5,27,31,2,31,13,35,1,35,5,39,1,39,5,43,2,13,43,47,2,47,10,51,1,51,6,55,2,55,9,59,1,59,5,63,1,63,13,67,2,67,6,71,1,71,5,75,1,75,5,79,1,79,9,83,1,10,83,87,1,87,10,91,1,91,9,95,1,10,95,99,1,10,99,103,2,103,10,107,1,107,9,111,2,6,111,115,1,5,115,119,2,119,13,123,1,6,123,127,2,9,127,131,1,131,5,135,1,135,13,139,1,139,10,143,1,2,143,147,1,147,10,0,99,2,0,14,0";
+const input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,10,19,23,1,23,9,27,1,5,27,31,2,31,13,35,1,35,5,39,1,39,5,43,2,13,43,47,2,47,10,51,1,51,6,55,2,55,9,59,1,59,5,63,1,63,13,67,2,67,6,71,1,71,5,75,1,75,5,79,1,79,9,83,1,10,83,87,1,87,10,91,1,91,9,95,1,10,95,99,1,10,99,103,2,103,10,107,1,107,9,111,2,6,111,115,1,5,115,119,2,119,13,123,1,6,123,127,2,9,127,131,1,131,5,135,1,135,13,139,1,139,10,143,1,2,143,147,1,147,10,0,99,2,0,14,0";
 
-let programArray = inputProgram.split(',')
+const nValuesInInstruction = 4; //Adding this in is maybe a case of YAGNI, but is is a "magic number" if you don't put it in?
+const outputTarget = 19690720;
 
-let position, programRunning, command, arg1Pos, arg2Pos, ansPos;
-if (programArray.length >0){
-    position = 0;
-    programRunning = true
+let memory
+let instructionPointer, programRunning, command, arg1Pos, arg2Pos, ansPos, output;
+
+for (let noun=0; noun<=99; noun++)
+{
+    for (let verb = 0; verb<=99; verb++){
+        memory = input.split(',')
+        memory[1] = noun;
+        memory[2] = verb;
+
+        if (memory.length >0){
+            instructionPointer = 0;
+            programRunning = true
+        }
+
+        while (programRunning) {
+            command = memory[instructionPointer];
+            if (command == 99) {
+                programRunning = false;
+            }
+            else if (command==1 || command == 2) {
+                arg1Pos = memory[instructionPointer+1]
+                arg2Pos = memory[instructionPointer+2]
+                ansPos = memory[instructionPointer+3]
+                if (command == 1) {
+                    memory[ansPos] = Number(memory[arg1Pos])+Number(memory[arg2Pos])
+                }
+                else {
+                    memory[ansPos] = Number(memory[arg1Pos])*Number(memory[arg2Pos])
+                }
+                instructionPointer += nValuesInInstruction;
+
+            }
+            else {
+                console.log(`Error - unrecognised command at position ${instructionPointer}`)
+                programRunning = false;
+            }
+        }
+
+        output = memory[0]
+
+        if (output == outputTarget)
+        {
+            console.log(`noun: ${noun}, verb = ${verb}`)
+        }
+    }
 }
 
-while (programRunning) {
-    command = programArray[position];
-    if (command == 99) {
-        programRunning = false;
-    }
-    else if (command==1 || command == 2) {
-        arg1Pos = programArray[position+1]
-        arg2Pos = programArray[position+2]
-        ansPos = programArray[position+3]
-        if (command == 1) {
-            programArray[ansPos] = Number(programArray[arg1Pos])+Number(programArray[arg2Pos])
-        }
-        else {
-            programArray[ansPos] = Number(programArray[arg1Pos])*Number(programArray[arg2Pos])
-        }
-        position = position+4;
 
-    }
-    else {
-        console.log(`Error - unrecognised command at position ${position}`)
-        programRunning = false;
-    }
-}
 
-const outputProgram = programArray.join(',')
-console.log(outputProgram)
-
-console.log(`Value at position 0: ${programArray[0]}`)
-
+// for noun = 12, verb = 2, output should be 4930687
+// for outputTarget = 19690720, noun should be 53, verb should be 35
